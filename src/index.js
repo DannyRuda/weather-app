0111da2ebb4313b68c0962b195574bd7662c355d
Danny Ruda <danny.ruda1997@gmail.com>
2022-11-22 00:10:20 +0100

    Now chains functions more intuitively
    Now manages to make the return value for a function from within
    a .then call in that function using new Promise and resolving in the
    then call rather than returning

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ function createWeatherObject(city,day,temp,feelslike,cond,humidity) {
 }
 
 function fillWeatherobjectWithData(weatherData) {
-    return createWeatherObject(weatherData.name, weatherData.dt,weatherData.main.temp, weatherData.main.feelslike, weatherData.weather[0].main, weatherData.main.humidity);
+    return createWeatherObject(weatherData.name, weatherData.dt,weatherData.main.temp, weatherData.main.feels_like, weatherData.weather[0].main, weatherData.main.humidity);
 }
 
 
 function getCurrentWeather(lat,lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d145974fac8fb803900422c2cc1d620e`)
-    .then((response)=>response.json())
-    .then((value)=>Promise.resolve(value))
-    .then((value) => { 
-        console.log(value)
-        console.log(fillWeatherobjectWithData(value))
+    return new Promise((resolve,reject)=>{
+        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d145974fac8fb803900422c2cc1d620e`)
+        .then((response)=>response.json())
+        .then((value)=>Promise.resolve(value))
+        .then((value) => { 
+            console.log(value)
+            console.log(fillWeatherobjectWithData(value))
+            resolve(value);
+        })
+        .catch((err)=>{reject(err)});
     })
 }
 
@@ -23,17 +27,25 @@ function getCityCoordinates(city) {
 }
 */ 
 
-function testGeocoding(cityName) {
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q={${cityName}}&appid=d145974fac8fb803900422c2cc1d620e`)
+async function testGeocoding(cityName) {
+    return new Promise((resolve,reject)=>{
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q={${cityName}}&appid=d145974fac8fb803900422c2cc1d620e`)
     .then((response) => {
         if (response.status === 200) {
             console.log('status was 200')
             return response.json();
         }
     })
-    .then( (value)=> {getCurrentWeather(value[0].lat,value[0].lon)} )
-    .catch((err)=>{ console.log(err) })
+    .then( (value)=> {
+        getCurrentWeather(value[0].lat,value[0].lon);
+        resolve([value[0].lat,value[0].lon]);
+    })
+    .catch((err)=>{ reject(err) })
+    })
 }
 
 testGeocoding('Herne')
+.then((value)=>getCurrentWeather(value[0],value[1]))
+.then((value)=>fillWeatherobjectWithData(value))
+.then((value)=>{console.log(value)})
 
