7433e1f0586ea6247cfdfae1722b75d8f067d054
Danny Ruda <danny.ruda1997@gmail.com>
2022-12-01 15:02:49 +0100

    seperates api calls from main script into a module

--- src/index.js
+++ src/index.js
@@ -1,71 +1,16 @@
-import {
-  createCurrentWeather,
-  createHourObject,
-  createDayObject,
-} from "./weatherObjects";
+import { writeWeatherintoObjects } from "./helperFunctions";
 
 import {
-  writeWeatherintoObjects,
-} from "./helperFunctions";
+  getCurrentWeather,
+  getFiveDayForecast,
+} from "./weatherAPI";
 
-function getCurrentWeather(city, country) {
-  return new Promise((resolve, reject) => {
-    getCityCoordinates(city, country).then((coordinates) => {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=d145974fac8fb803900422c2cc1d620e`
-      )
-        .then((response) => response.json())
-        .then((value) => Promise.resolve(value))
-        .then((value) => {
-          resolve(value);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  });
-}
-
-function getFiveDayForecast(city,country) {
-  return new Promise((resolve, reject) => {
-    getCityCoordinates(city, country).then((coordinates) => {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=d145974fac8fb803900422c2cc1d620e`
-      )
-        .then((response) => response.json())
-        .then((value) => Promise.resolve(value))
-        .then((value) => {
-          resolve(value);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  });
-}
-
-function getCityCoordinates(cityName, country) {
-  return new Promise((resolve, reject) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q={${cityName}},{${country}}&appid=d145974fac8fb803900422c2cc1d620e`
-    )
-      .then((response) => {
-        if (response.status === 200) {
-          console.log("status was 200");
-          return response.json();
-        }
-      })
-      .then((value) => {
-        resolve([value[0].lat, value[0].lon]);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
-}
-
-writeWeatherintoObjects(getCurrentWeather("duisburg","DE"),getFiveDayForecast("duisburg","DE"))
-.then((weatherObjects)=>{console.log(weatherObjects)});
+writeWeatherintoObjects(
+  getCurrentWeather("duisburg", "DE"),
+  getFiveDayForecast("duisburg", "DE")
+).then((weatherObjects) => {
+  console.log(weatherObjects);
+});
 /*
 function testGeocoding(cityName) {
   return new Promise((resolve, reject) => {
---
+++ src/weatherAPI.js
@@ -0,0 +1,57 @@
+export { getCityCoordinates, getCurrentWeather, getFiveDayForecast };
+
+function getCityCoordinates(cityName, country) {
+  return new Promise((resolve, reject) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q={${cityName}},{${country}}&appid=d145974fac8fb803900422c2cc1d620e`
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("status was 200");
+          return response.json();
+        }
+      })
+      .then((value) => {
+        resolve([value[0].lat, value[0].lon]);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+function getCurrentWeather(city, country) {
+  return new Promise((resolve, reject) => {
+    getCityCoordinates(city, country).then((coordinates) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=d145974fac8fb803900422c2cc1d620e`
+      )
+        .then((response) => response.json())
+        .then((value) => Promise.resolve(value))
+        .then((value) => {
+          resolve(value);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+}
+
+function getFiveDayForecast(city, country) {
+  return new Promise((resolve, reject) => {
+    getCityCoordinates(city, country).then((coordinates) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates[0]}&lon=${coordinates[1]}&appid=d145974fac8fb803900422c2cc1d620e`
+      )
+        .then((response) => response.json())
+        .then((value) => Promise.resolve(value))
+        .then((value) => {
+          resolve(value);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  });
+}
